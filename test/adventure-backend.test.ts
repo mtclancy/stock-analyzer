8ac17fc827d358e5cdcc51364cffccc0a8dead7b
Mike <mclancy@leantechniques.com>
2024-09-21 14:01:52 -0700

    Initial commit

---
+++ test/adventure-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AdventureBackend from '../lib/adventure-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/adventure-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AdventureBackend.AdventureBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
