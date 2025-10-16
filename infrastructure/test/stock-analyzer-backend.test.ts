f85110c74bcb6142b8240d68a99126f5e7ab8d2f
mtclancy <mtclancy86@gmail.com>
2025-10-16 12:12:28 -0500

    renamed project

--- infrastructure/test/stock-analyzer-backend.test.ts
+++ infrastructure/test/stock-analyzer-backend.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as AdventureBackend from '../lib/adventure-backend-stack';
+// import * as AdventureBackend from '../lib/stock-analyzer-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/adventure-backend-stack.ts
+// example resource in lib/stock-analyzer-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
